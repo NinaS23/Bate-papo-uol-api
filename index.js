ac9f3a37afe85337f54b732b4b522915c5f9752b
MariaDuda <mariabevenuto062@gmail.com>
2022-06-21 13:10:47 -0300

    post from ednPoint participants , going fine

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 import express , {json} from "express"
 import cors from "cors"
-import chalk from "chalk"
+import dotenv from "dotenv"
+import joi from "joi"
 
 const PORT = 5000
+const string = joi.object({
+    name: joi.string().required()
+  })
+  
+
+const dontev = dotenv.config();
+console.log(dontev)
 const app = express()
 app.use(cors())
 app.use(json())
 
+app.post("/participants" , (req,res)=>{
+    const participante = req.body
+    console.log(participante)
+     
+})
 
 
 app.listen(PORT , ()=>{
