cb40b53b9979c728ad3f9e68ba566a497c21d6d3
MariaDuda <mariabevenuto062@gmail.com>
2022-06-21 13:27:46 -0300

    add a good name for a variable

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ const string = joi.object({
   
 
 const dontev = dotenv.config();
-console.log(dontev)
 const app = express()
 app.use(cors())
 app.use(json())
 
 app.post("/participants" , (req,res)=>{
-    const participante = req.body
-    console.log(participante)
+    const username= req.body
+    console.log(username)
+
      
 })
 
