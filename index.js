cc7226c7c2a053a6d901d60d55fa40aa5e58700d
MariaDuda <mariabevenuto062@gmail.com>
2022-06-22 14:38:36 -0300

    post from participants

--- index.js
+++ index.js
@@ -23,49 +23,62 @@ promise.catch(res => console.log(chalk.red("deu xabu"), res))
 
 
 
-const schema = joi.object({
-    name: joi.string().min(1).required()
+const schemaNameValidate = joi.object({
+    name:joi.string().alphanum().required(),
 })
-
 
 app.post("/participants", async (req, res) => {
-    const user = req.body
-    console.log(user)
-
-    const validateName = schema.validate(user)
-   
+    const { name } = req.body;
+  
+    const schemaNameValidate= joi.object({
+      name: joi.string().min(1).required(),
+    });
+  
+    const validateName = schemaNameValidate.validate({ name });
+  
     if (validateName.error) {
-        res.status(422)
+      res.sendStatus(422);
+      return;
     }
+  
+    try {
+      const user = await database.collection("participants").findOne({ name });
+      console.log(user)
+  
+      if (user) {
+        res.sendStatus(409);
+        return;
+      } else {
+        await db .collection("participants") .insertOne({ name, lastStatus: Date.now() });
+        db.collection("participants").find().toArray().then(users => {
+            console.log(users); 
+        });
+
+        await database.collection("messages").insertOne({
+          from: name,
+          to: "Todos",
+          text: "entra na sala...",
+          type: "status",
+          time: dayjs().format("HH:mm:ss"),
+        });
+      }
+    } catch (e) {
+      console.log(e);
+    }
+    res.sendStatus(201);
+  });
+  
+
+app.get("/participants" , async (req,res)=>{
     try{
-       const users = mongoClient.db("batepapo-uol").collection("users")
-       const messages = mongoClient.db.apply("batepapo-uol").collection("messages")
-       const findUser = await users.findOne({name: user.name})
-       console.log(findUser)
-       if(findUser){
-           res.status(409)
-           return;
-       }
-          console.log(mongoClient.db(users).find())
-          await users.insertOne({name: [...user] , lastStatus: Date.now() })
-          const message =await messages.insertOne({
-              from: [...user.name],
-              to:"todos",
-              text:'entra na sala...',
-              type: 'status',
-              time: dayjs().format("HH:mm:ss")
-          }) 
-          console.log(message)
-          res.sendStatus(201)
-          mongoClient.close()
-    }catch (error){
-      mongoClient.close()
+        let participants = await mongoClient.db.collection("participants").find().toArray();
+        console.log(participants)
+        res.status(200).send(participants);
+    }catch(e){
+        res.status(404).send("desculpe, mas não conseguimos achar o participante");
+        mongoClient.close();
     }
-
-   
-
 })
-
 
 app.listen(5000 , ()=>{
     console.log("wake")
