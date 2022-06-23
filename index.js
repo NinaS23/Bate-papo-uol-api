fbfa4510612547703dc502ade6797aa9a6a61547
MariaDuda <mariabevenuto062@gmail.com>
2022-06-23 12:28:56 -0300

    limit on get messages done

--- index.js
+++ index.js
@@ -25,57 +25,57 @@ promise.catch(res => console.log(chalk.red("deu xabu"), res))
 
 
 app.post("/participants", async (req, res) => {
-    const { name } = req.body;
-  
-    const schemaNameValidate= joi.object({
-      name: joi.string().required(),
-    });
-  
-    const validateName = schemaNameValidate.validate({ name });
-  
-    if (validateName.error) {
-      res.sendStatus(422);
-      return;
-    }
-  
-    try {
+  const { name } = req.body;
 
-      const user = await db.collection("participants").findOne({ name });
-      db.collection("participants").find().toArray().then(users => {
-        console.log(users); 
-    });
-      console.log(user)
-  
-      if (user) {
-        res.sendStatus(409);
-        return;
-
-      } else {
-        await db .collection("participants").insertOne({ name, lastStatus: Date.now() });
-        db.collection("participants").find().toArray().then(users => {
-            console.log(users); 
-        });
-
-        await db.collection("messages").insertOne({
-          from: name,
-          to: "Todos",
-          text: "entra na sala...",
-          type: "status",
-          time: dayjs().format("HH:mm:ss"),
-        });
-      }
-    } catch (e) {
-      console.log(e);
-      mongoClient.close()
-    }
-    res.sendStatus(201);
-    mongoClient.close()
+  const schemaNameValidate = joi.object({
+    name: joi.string().required(),
   });
-  
 
-app.get("/participants" , async (req,res)=>{
+  const validateName = schemaNameValidate.validate({ name });
 
-  try{
+  if (validateName.error) {
+    res.sendStatus(422);
+    return;
+  }
+
+  try {
+
+    const user = await db.collection("participants").findOne({ name });
+    db.collection("participants").find().toArray().then(users => {
+      console.log(users);
+    });
+    console.log(user)
+
+    if (user) {
+      res.sendStatus(409);
+      return;
+
+    } else {
+      await db.collection("participants").insertOne({ name, lastStatus: Date.now() });
+      db.collection("participants").find().toArray().then(users => {
+        console.log(users);
+      });
+
+      await db.collection("messages").insertOne({
+        from: name,
+        to: "Todos",
+        text: "entra na sala...",
+        type: "status",
+        time: dayjs().format("HH:mm:ss"),
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    mongoClient.close()
+  }
+  res.sendStatus(201);
+  mongoClient.close()
+});
+
+
+app.get("/participants", async (req, res) => {
+
+  try {
     const Allparticipant = db.collection("participants").find().toArray()
     res.status(200).send(Allparticipant);
     mongoClient.close();
@@ -83,49 +83,69 @@ app.get("/participants" , async (req,res)=>{
     res.send(404).send("desculpe, mas não conseguimos achar o participante");
     mongoClient.close();
   }
-  
+
 })
-app.post("messages" , async (req , res) =>{
-  const { to , text , type } = req.body
+app.post("messages", async (req, res) => {
+  const { to, text, type } = req.body
   const participant = req.header.user;
 
   const schemaMessage = joi.object({
-    to:joi.string().required(),
-    text:joi.string().required(),
+    to: joi.string().required(),
+    text: joi.string().required(),
     type: joi.string().allow('message', 'private_message'),
   })
-  
-    const validateMessage =  schemaMessage.validate({ to,type,text} , { abortEarly: false });
-    if(!validateMessage){
-      return res.send(422);
-    }else{
-      sendStatus(422).send("xabu", error)
-    }
-  
-    try{
-      const user = db.collection("participants").findOne({participant})
-      if (!user) {
-        res.sendStatus(422);
-        return;
-      } else {
-        await database.collection("messages").insertOne({
-          from: user.name,
-          to,
-          text,
-          type,
-          time: dayjs().format("HH:mm:ss"),
-        });
-        res.sendStatus(201);
-      }
- 
-    }catch(e){
-      console.log(e)
-    }
-   
-  
 
+  const validateMessage = schemaMessage.validate({ to, type, text }, { abortEarly: false });
+  if (!validateMessage) {
+    return res.send(422);
+  } else {
+    sendStatus(422).send("xabu", error)
+  }
+
+  try {
+    const user = db.collection("participants").findOne({ participant })
+    if (!user) {
+      res.sendStatus(422);
+      return;
+    } else {
+      await database.collection("messages").insertOne({
+        from: user.name,
+        to,
+        text,
+        type,
+        time: dayjs().format("HH:mm:ss"),
+      });
+      res.sendStatus(201);
+    }
+
+  } catch (e) {
+    console.log(e)
+  }
 })
 
+app.get("messages", async (req, res) => {
+  const limit = parseInt(req.query.limit)
+  const user = req.headers.user
+  let arrAux = []
+  try {
+    const messages = await database.collection("messages").find({}).toArray()
+    for (let i = 0; i < messages.length; i++) {
+      if (messages[i].to === user || messages[i].from === user) {
+        arrAux.push(messages[i])
+      }
+    }
+    if (!limit) {
+      res.send(arrAux)
+    } else {
+      let reverseMessages = [...arrAux].reverse()
+      let limitMessages = reverseMessages.splice(0, limit)
+      res.send(limitMessages)
+    }
+  } catch (e) {
+    console.log(e)
+  }
+
+})
 
 
 app.listen(5000 , ()=>{
